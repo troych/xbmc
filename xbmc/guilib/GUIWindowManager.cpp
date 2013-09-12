c45dda14d5df80e029dea1fd03ec4a0ba8ce4bc3
Garrett Brown <garbearucla@gmail.com>
2013-09-12 13:57:12 -0700

    [retroplayer] - (0007) MyGames window

--- xbmc/guilib/GUIWindowManager.cpp
+++ xbmc/guilib/GUIWindowManager.cpp
@@ -150,6 +150,7 @@
 /* Game related include files */
 #include "games/controllers/windows/GUIControllerWindow.h"
 #include "games/dialogs/GUIDialogSavestates.h"
+#include "games/windows/GUIWindowGames.h"
 
 using namespace PVR;
 using namespace PERIPHERALS;
@@ -303,6 +304,7 @@ void CGUIWindowManager::CreateWindows()
 
   Add(new GAME::CGUIControllerWindow);
   Add(new GAME::CGUIDialogSavestates);
+  Add(new GAME::CGUIWindowGames);
 }
 
 bool CGUIWindowManager::DestroyWindows()
@@ -408,6 +410,7 @@ bool CGUIWindowManager::DestroyWindows()
     Delete(WINDOW_WEATHER);
     Delete(WINDOW_DIALOG_GAME_CONTROLLERS);
     Delete(WINDOW_DIALOG_SAVESTATES);
+    Delete(WINDOW_GAMES);
 
     Remove(WINDOW_SETTINGS_SERVICE);
     Remove(WINDOW_SETTINGS_MYPVR);
