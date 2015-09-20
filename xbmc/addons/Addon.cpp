25b7e67476ed9c82866759072f408bdcd3e21155
Garrett Brown <themagnificentmrb@gmail.com>
2015-09-20 13:07:53 -0700

    [addons] Try binaries path if library path doesn't exist
    This will fix cases e.g. when the add-on's path is in:
    
    /usr/share/kodi/addons/
    
    but the add-on's binary path is in:
    
    /usr/lib/kodi/addons/

--- xbmc/addons/Addon.cpp
+++ xbmc/addons/Addon.cpp
@@ -25,6 +25,7 @@
 #include "settings/Settings.h"
 #include "filesystem/Directory.h"
 #include "filesystem/File.h"
+#include "filesystem/SpecialProtocol.h"
 #include "system.h"
 #ifdef HAS_PYTHON
 #include "interfaces/python/XBPython.h"
@@ -617,7 +618,18 @@ const std::string CAddon::Icon() const
 
 const std::string CAddon::LibPath() const
 {
-  return URIUtils::AddFileToFolder(m_props.path, m_strLibName);
+  std::string strLibPath = URIUtils::AddFileToFolder(m_props.path, m_strLibName);
+
+  // Check if add-on library has been installed to the binaries path instead
+  std::string strSharePath = CSpecialProtocol::TranslatePath("special://xbmc/");
+  const bool bIsInSharePath = StringUtils::StartsWith(strLibPath, strSharePath);
+  if (bIsInSharePath && !CFile::Exists(strLibPath))
+  {
+    std::string strBinPath = CSpecialProtocol::TranslatePath("special://xbmcbin/");
+    strLibPath.replace(0, strSharePath.length(), strBinPath);
+  }
+
+  return strLibPath;
 }
 
 AddonVersion CAddon::GetDependencyVersion(const std::string &dependencyID) const
