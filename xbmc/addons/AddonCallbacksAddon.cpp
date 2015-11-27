9f1eee0ddda1b6df4b7a501a33eb651d3bac5f12
notspiff <cptspiff@gmail.com>
2015-11-27 15:24:26 +0100

    [PR 8450] added: add-on callback for listing a directory

--- xbmc/addons/AddonCallbacksAddon.cpp
+++ xbmc/addons/AddonCallbacksAddon.cpp
@@ -31,6 +31,7 @@
 #include "utils/CharsetConverter.h"
 #include "utils/StringUtils.h"
 #include "utils/XMLUtils.h"
+#include "kodi_vfs_types.h"
 
 using namespace XFILE;
 
@@ -524,6 +525,48 @@ bool CAddonCallbacksAddon::RemoveDirectory(const void* addonData, const char *st
     CFile::Delete(fileItems.Get(i)->GetPath());
 
   return CDirectory::Remove(strPath);
+}
+
+static void CFileItemListToVFSDirEntries(VFSDirEntry* entries,
+                                         const CFileItemList& items)
+{
+  for (int i=0;i<items.Size();++i)
+  {
+    entries[i].label = strdup(items[i]->GetLabel().c_str());
+    entries[i].path = strdup(items[i]->GetPath().c_str());
+    entries[i].size = items[i]->m_dwSize;
+    entries[i].folder = items[i]->m_bIsFolder;
+  }
+}
+
+bool CAddonCallbacksAddon::GetDirectory(const void* addonData, const char *strPath, const char* mask, VFSDirEntry** items, int* num_items)
+{
+  CAddonCallbacks* helper = (CAddonCallbacks*) addonData;
+  if (!helper)
+    return false;
+
+  CFileItemList fileItems;
+  if (!CDirectory::GetDirectory(strPath, fileItems, mask, DIR_FLAG_NO_FILE_DIRS))
+    return false;
+
+  *num_items = fileItems.Size();
+  *items = new VFSDirEntry[fileItems.Size()];
+  CFileItemListToVFSDirEntries(*items, fileItems);
+  return true;
+}
+
+void CAddonCallbacksAddon::FreeDirectory(const void* addonData, VFSDirEntry* items, int num_items)
+{
+  CAddonCallbacks* helper = (CAddonCallbacks*) addonData;
+  if (!helper)
+    return;
+
+  for (int i=0;i<num_items;++i)
+  {
+    free(items[i].label);
+    free(items[i].path);
+  }
+  delete[] items;
 }
 
 }; /* namespace ADDON */
---
+++ xbmc/addons/include/kodi_vfs_types.h
@@ -0,0 +1,41 @@
+/*
+ *      Copyright (C) 2015 Team Kodi
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Kodi; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+#pragma once
+
+#include <stdint.h>
+
+extern "C"
+{
+  struct VFSProperty
+  {
+    char* name;
+    char* val;
+  };
+
+  struct VFSDirEntry
+  {
+    char* label;             //!< item label
+    char* title;             //!< item title
+    char* path;              //!< item path
+    int num_props;           //!< Number of properties attached to item
+    VFSProperty* properties; //!< Properties
+    bool folder;             //!< Item is a folder
+    uint64_t size;           //!< Size of file represented by item
+  };
+}
