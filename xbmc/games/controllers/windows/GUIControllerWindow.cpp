df9ff71701ec6d35a6b78e9c328628d8e337c7a5
montellese <montellese@kodi.tv>
2016-02-06 12:13:28 -0800

    CGUIControllerWindow: fix focusing of DialogOK if no peripheral addon is available

--- xbmc/games/controllers/windows/GUIControllerWindow.cpp
+++ xbmc/games/controllers/windows/GUIControllerWindow.cpp
@@ -27,7 +27,6 @@
 #include "addons/IAddon.h"
 #include "guilib/GUIMessage.h"
 #include "guilib/WindowIDs.h"
-#include "messaging/ApplicationMessenger.h"
 
 // To check for button mapping support
 #include "dialogs/GUIDialogOK.h"
@@ -145,7 +144,6 @@ void CGUIControllerWindow::Notify(const Observable &obs, const ObservableMessage
 
 void CGUIControllerWindow::OnInitWindow(void)
 {
-  using namespace KODI::MESSAGING;
   using namespace PERIPHERALS;
 
   CGUIDialog::OnInitWindow();
@@ -172,7 +170,7 @@ void CGUIControllerWindow::OnInitWindow(void)
 
   // Focus the first controller so that the feature list is loaded properly
   CGUIMessage msgFocus(GUI_MSG_SETFOCUS, GetID(), CONTROL_CONTROLLER_BUTTONS_START);
-  CApplicationMessenger::GetInstance().SendGUIMessage(msgFocus, WINDOW_INVALID, false);
+  OnMessage(msgFocus);
 
   // Check for button mapping support (TODO: remove this)
   PeripheralBusAddonPtr bus = std::static_pointer_cast<CPeripheralBusAddon>(g_peripherals.GetBusByType(PERIPHERAL_BUS_ADDON));
