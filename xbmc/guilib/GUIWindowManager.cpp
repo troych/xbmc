8913753a9e1c3da4936646176073c1e47ebf2d13
Garrett Brown <themagnificentmrb@gmail.com>
2016-05-05 00:26:37 -0700

    [PR 11034] Savestate manager

--- xbmc/guilib/GUIWindowManager.cpp
+++ xbmc/guilib/GUIWindowManager.cpp
@@ -149,6 +149,7 @@
 
 /* Game related include files */
 #include "games/controllers/windows/GUIControllerWindow.h"
+#include "games/dialogs/GUIDialogSavestates.h"
 #include "games/windows/GUIWindowGames.h"
 
 using namespace PVR;
@@ -302,6 +303,7 @@ void CGUIWindowManager::CreateWindows()
   Add(new CGUIWindowEventLog);
 
   Add(new GAME::CGUIControllerWindow);
+  Add(new GAME::CGUIDialogSavestates);
   Add(new GAME::CGUIWindowGames);
 }
 
@@ -407,6 +409,7 @@ bool CGUIWindowManager::DestroyWindows()
     Delete(WINDOW_PICTURES);
     Delete(WINDOW_WEATHER);
     Delete(WINDOW_DIALOG_GAME_CONTROLLERS);
+    Delete(WINDOW_DIALOG_SAVESTATES);
     Delete(WINDOW_GAMES);
 
     Remove(WINDOW_SETTINGS_SERVICE);
