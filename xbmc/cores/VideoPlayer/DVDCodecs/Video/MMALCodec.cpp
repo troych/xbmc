c6f60210201aca660626eeb696d1068104b078d7
popcornmix <popcornmix@gmail.com>
2016-07-19 20:13:39 +0100

    mmal: Remove renderer pointer from codecs

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/MMALCodec.cpp
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/MMALCodec.cpp
@@ -102,7 +102,6 @@ CMMALVideo::CMMALVideo(CProcessInfo &processInfo) : CDVDVideoCodec(processInfo)
   m_dec_input = NULL;
   m_dec_output = NULL;
   m_dec_input_pool = NULL;
-  m_renderer = NULL;
   m_pool = nullptr;
 
   m_codingType = 0;
@@ -351,7 +350,6 @@ bool CMMALVideo::Open(CDVDStreamInfo &hints, CDVDCodecOptions &options)
     return false;
 
   m_hints = hints;
-  m_renderer = (CMMALRenderer *)options.m_opaque_pointer;
   MMAL_STATUS_T status;
 
   m_decoded_width = hints.width;
