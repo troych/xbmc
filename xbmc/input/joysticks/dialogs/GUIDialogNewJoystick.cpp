cbadd39ee3e0e46002901af4758b5d74febe5781
Garrett Brown <themagnificentmrb@gmail.com>
2016-10-19 08:29:57 -0700

    [PR 10882] Show controller dialog if the user connects an unmapped controller

---
+++ xbmc/input/joysticks/dialogs/GUIDialogNewJoystick.cpp
@@ -0,0 +1,61 @@
+/*
+ *      Copyright (C) 2016 Team Kodi
+ *      http://kodi.tv
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this Program; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "GUIDialogNewJoystick.h"
+#include "dialogs/GUIDialogYesNo.h"
+#include "guilib/GUIWindowManager.h"
+#include "guilib/WindowIDs.h"
+#include "settings/Settings.h"
+
+using namespace JOYSTICK;
+
+CGUIDialogNewJoystick::CGUIDialogNewJoystick() :
+  CThread("NewJoystickDlg")
+{
+}
+
+void CGUIDialogNewJoystick::ShowAsync()
+{
+  bool bShow = true;
+
+  if (IsRunning())
+    bShow = false;
+  else if (!CSettings::GetInstance().GetBool(CSettings::SETTING_INPUT_ASKNEWCONTROLLERS))
+    bShow = false;
+  else if (g_windowManager.GetActiveWindow() == WINDOW_DIALOG_GAME_CONTROLLERS)
+    bShow = false;
+
+  if (bShow)
+    Create();
+}
+
+void CGUIDialogNewJoystick::Process()
+{
+  // "New controller detected"
+  // "Would you like to configure your controller? This can also be done in System Settings."
+  if (CGUIDialogYesNo::ShowAndGetInput(CVariant{ 35011 }, CVariant{ 35012 }))
+  {
+    g_windowManager.ActivateWindow(WINDOW_DIALOG_GAME_CONTROLLERS);
+  }
+  else
+  {
+    CSettings::GetInstance().SetBool(CSettings::SETTING_INPUT_ASKNEWCONTROLLERS, false);
+  }
+}
